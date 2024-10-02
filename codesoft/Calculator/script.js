ff4ab1b4735d3ee234aa96b345d9f029983fef2c
mamidivijaysimha <163118995+mamidivijaysimha@users.noreply.github.com>
2024-10-02 09:47:09 -0700

    Add files via upload

---
+++ codesoft/Calculator/script.js
@@ -0,0 +1,16 @@
+function display(value) {
+    document.getElementById('display').value += value;
+}
+
+function clear() {
+    document.getElementById('display').value = '';
+}
+
+function calculate() {
+    const display = document.getElementById('display');
+    try {
+        display.value = eval(display.value);
+    } catch (error) {
+        display.value = 'Error';
+    }
+}
